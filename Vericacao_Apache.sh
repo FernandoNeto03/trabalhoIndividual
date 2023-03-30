aad36bac1753c48ef9aefeae1b05d2ee0d65b50b
FernandoNeto03 <serafinineto@gmail.com>
2023-03-30 19:46:21 -0300

    Criei aqui o script para mostrar a hora atual e a verificação do Apache, se está funcionando ou não.

---
+++ Vericacao_Apache.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#Mostrar data e hora atual
+horarioAtual=$(date +"%Y-%m-%d %H:%M:%S")
+
+#Verificar se o servidor Apache está funcionando
+status_Servidor=" "
+
+if systemctl is-active --quiet httpd; then
+	status_Servidor="online"
+	mensagem_Personalizada="Eba! O seu servidor está online e funcionando!"
+else
+	status_Servidor="offline"
+	mensagem_Personalizada="Ops! O seu servidor está offline e não está funcionando."
+fi
+
+#Resposta completa com informações
+resposta_Log="\nHora Atual: ${horarioAtual} \nStatus do Apache: ${status_Servidor} ${mensagem_Personalizada}"
